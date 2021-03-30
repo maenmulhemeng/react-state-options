4920dbe66672c4861dd4efd546a67c073963e588
maenmulhem <maen@gta-canada.com>
2021-03-30 10:35:33 -0600

    hotfix

--- src/components/forms/add/AddCustomerForm.js
+++ src/components/forms/add/AddCustomerForm.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 
 const AddCustomerForm = () => {
     const { register, handleSubmit, watch, errors } = useForm();
     const onSubmit = data => console.log(data);
-    console.log(watch("example")); 
+    
+    //console.log(watch("example")); 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
                <h1>This form is built for the sake of demo and need to be done later</h1>
+              
         </form>
     );
   }
